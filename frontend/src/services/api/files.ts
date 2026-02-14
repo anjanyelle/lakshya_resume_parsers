fc7e75ae6879fc686607ec183d03df900c8a3793
anjanyelles <anjanyelle@gmail.com>
2026-02-14 19:17:31 +0530

    ch

--- frontend/src/services/api/files.ts
+++
@@ -1,6 +0,0 @@
-import { apiClient } from './client'
-
-export const fetchFileAsBlobUrl = async (url: string) => {
-  const response = await apiClient.get<Blob>(url, { responseType: 'blob' })
-  return URL.createObjectURL(response.data)
-}
